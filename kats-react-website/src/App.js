e5981b282a4c4866e46089e495b66c8ba4260e3f
Superkat32 <superkat1806@gmail.com>
2023-08-05 22:40:34 -0400

    Fixed GitHub Pages
    FIX: Fixed GitHub Pages not showing anything and or 404'ing

--- kats-react-website/src/App.js
+++ kats-react-website/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 
 function AboutPage() {
   return (
@@ -104,6 +103,7 @@ function ModrinthIcon() {
 }
 
 
+
 export default function App() {
   return (
     <div className="App">
@@ -112,11 +112,4 @@ export default function App() {
     </div>
   );
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
