b11b17986b72f71dffb906cb44aee901f56b6be8
Superkat32 <superkat1806@gmail.com>
2023-08-11 00:13:26 -0400

    Inner peace
    FEAT: Began working on the projects section! I love the way it looks!
    FEAT: A project can be added by simply filling out a little bit of information in a json like style. The icons will be reworked to be included in the files(I'm lazy right now)
    REFACTOR: Added margins to the main header

--- kats-react-website/src/App.js
+++ kats-react-website/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react';
 
+// First highlighted ara
+
 function AboutPage() {
   return (
     <div className="fullScreenColoredSection">
@@ -102,13 +104,76 @@ function ModrinthIcon() {
   )
 }
 
+const PROJECTS = [
+  {project: "Broken Lead Warner", category: "fabricmod", icon: "https://cdn.modrinth.com/data/29RFArLV/d29ad4c8448761c3db2e5c2759bc541c5df6d68a.png", imgalt: "Broken Lead Warner icon", desc: "A simple mod which alerts you if your lead breaks."},
+  {project: "Explosive Enhancement", category: "fabricmod", icon: "https://cdn.modrinth.com/data/OSQ8mw2r/88f44e9ed022061b1eb865996b2f98fe01937de6.png", imgalt: "Explosive Enhancement icon", desc: "Makes the explosion effect cooler! Even includes an API!"},
+  {project: "Postmortal Particles", category: "fabricmod", icon: "https://cdn.modrinth.com/data/5km6gS6P/856c44060b5570eb1278c1ac18bc9b2db10754ea.png", imgalt: "Postmortal Particles icon", desc: "Overhauls the totem of undying effect! "},
+  {project: "Sparking Strikes", category: "fabricmod", icon: "https://cdn.modrinth.com/data/y3xdomkH/ac8c3c9da8b58ac27c7925469575a0afd984d239.png", imgalt: "Sparkling Strikes icon", desc: "Adds fun, customizable particles whenever landing a strike on a foe!"},
+  {project: "You Were Slain", category: "fabricmod", icon: "https://cdn.modrinth.com/data/SfSneJdD/029b73b8938990803ae43803e6e2896ad15aeee6.png", imgalt: "You Were Slain icon", desc: "Customize the death screen unlike ever before!"},
+  {project: "Hampter", category: "fabricmod", icon: "https://cdn.modrinth.com/data/9UqguhZC/a9cb1208f61d68baedc8f40cb81ede63375d309b.png", imgalt: "Hampter icon", desc: "Adds the one and only Hampter to Minecraft!"}
+]
 
+
+// Second highlighted ara
+function MyProjects({ projects }) {
+  const myprojects = [];
+  projects.forEach((project) => {
+    myprojects.push(
+      // <ProjectIcon icon={project.icon} key={project.icon} />
+      <Project project={project} key={project} />
+    );
+  });
+
+  return (
+    <div className="fullScreenColoredSection">
+      {/* <h1 className="header">My Projects</h1>
+      <h3>All here for you to see!</h3> */}
+      <h1>Minecraft Fabric Mods</h1>
+      <div className="grid">
+          {myprojects}
+      </div>
+    </div>
+  )
+}
+
+function Project({ project }) {
+  return(
+    <>
+    <div>
+      <div>
+        <ProjectIcon icon={project.icon} imgalt={project.imgalt} key={project} />
+      </div>
+      <div className="avatarCompatSection">
+        <h4>{project.project}</h4>
+        <p>{project.desc}</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+function ProjectIcon({ icon, imgalt }) {
+  return(
+    <>
+    <div className="avatarColoredSection">
+      <img 
+      className='avatar'
+      src={icon}
+      alt={imgalt}
+      />
+    </div>
+    </>
+  )
+}
 
 export default function App() {
   return (
     <div className="App">
       <h1 className="header">Superkat's Awesome Website</h1>
       <AboutPage />
+      <h1 className="header">My Projects</h1>
+      <h3>All here for you to see!</h3>
+      <MyProjects projects={PROJECTS} />
     </div>
   );
 }
